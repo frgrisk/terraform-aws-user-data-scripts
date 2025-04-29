44f2db04d603bf29fe03ed3ded7adaedad9075da
Henry Skiba <henry.skiba@frgrisk.com>
2025-04-29 09:45:47 +0800

    Refactor output to use template files for base script generation with package update options (#4)
    * Refactor output to use template files for base script generation with package update options
    
    * support RHEL 9 for password login, fix package update script

--- scripts/password_login.sh
+++ scripts/password_login.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-systemctl restart sssd
+if [ -f /etc/ssh/sshd_config.d/50-cloud-init.conf ]; then
+  sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config.d/50-cloud-init.conf
+fi
+systemctl restart sshd
